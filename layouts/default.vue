e582a1ab4173c466ad48f6ffa3100e05567521d8
Rakantor <rakantor.dev@gmail.com>
2022-10-19 21:08:52 +0200

    Add logout button to app bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,13 @@
   <v-app dark>
     <v-app-bar fixed app>
       <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn v-if="loggedIn" icon color="primary" @click="logout">
+        <v-icon>mdi-logout-variant</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-main>
-      <v-container fill-height>
-        <Nuxt />
-      </v-container>
+      <Nuxt />
     </v-main>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
@@ -16,12 +18,31 @@
 </template>
 
 <script>
+import { signOut } from 'firebase/auth'
+
 export default {
   name: 'DefaultLayout',
   data() {
     return {
       fixed: false,
       title: 'IU Quiz App',
+    }
+  },
+  computed: {
+    loggedIn () {
+      return this.$store.state.userLoggedIn
+    }
+  },
+  methods: {
+    logout () {
+      signOut(this.$auth).then(() => {
+        // Sign-out successful
+        // The authentication state observer will redirect the user to the main page (dashboard),
+        // see pages/index.vue
+      }).catch((error) => {
+        // An error happened.
+        this.$toast({ content: error, color: 'error' })
+      })
     }
   }
 }
