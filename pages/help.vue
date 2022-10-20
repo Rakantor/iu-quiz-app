cd485631d87ad69fe49e5d7f53a5a917abe98b05
Rakantor <rakantor.dev@gmail.com>
2022-10-20 14:12:46 +0200

    Add help page

---
+++ pages/help.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" class="text-center">
+        <blockquote class="blockquote">
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HelpPage',
+  layout ({ $auth }) {
+    // Ref: https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#currentuser
+    return $auth.currentUser.emailVerified ? 'default' : 'unverified'
+  },
+}
+</script>
