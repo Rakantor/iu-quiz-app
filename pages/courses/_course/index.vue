bdbeb5172854c4c7e64d848a04180832eb8d14d2
Rakantor <rakantor.dev@gmail.com>
2022-10-28 21:48:32 +0200

    Add index.vue

---
+++ pages/courses/_course/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="auto">
+        <v-btn depressed color="primary" @click="playVersus">Challenge Mode</v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn depressed color="primary" @click="playCoop">Co-op Mode</v-btn>
+      </v-col>
+      <v-col cols="12">
+        <AddClosedEndedQuestion />
+      </v-col>
+      <v-col cols="12">
+        <AddOpenEndedQuestion />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'CourseIndexPage',
+  data() {
+    return {
+      courseID: undefined
+    }
+  },
+  computed: {
+  },
+  created () {
+    this.courseID = this.$route.params.course
+  },
+  methods: {
+    playVersus () {
+      // TODO
+      this.$router.push(`${this.$route.path}/play`)
+    },
+    playCoop () {
+      // TODO
+      this.$toast({ content: 'Todo: Co-op Mode implementieren', color: 'info' })
+    }
+  }
+}
+</script>
