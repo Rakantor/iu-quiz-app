1588d8a4d403527226006101fba8e5ff0129c878
Rakantor <rakantor.dev@gmail.com>
2022-11-06 19:13:24 +0100

    Change document title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - iu-quiz-app',
-    title: 'iu-quiz-app',
+    titleTemplate: '%s',
+    title: 'Quiz App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
