3dc09aff13d458837fe2c3a5689a830969e8aa67
Rakantor <rakantor.dev@gmail.com>
2022-11-08 23:04:03 +0100

    Add help page link to footer

--- components/FooterApp.vue
+++ components/FooterApp.vue
@@ -3,6 +3,7 @@
     <v-row justify="center" no-gutters>
       <v-col cols="12" class="text-center">
         <span class="text-caption">&copy; {{ new Date().getFullYear() }}</span>
+        <v-btn dark small text rounded :to="{ name: 'help' }">FAQ</v-btn>
       </v-col>
     </v-row>
   </v-footer>
