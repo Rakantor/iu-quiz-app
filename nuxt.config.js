858e2024b828bdb71e82b6fe58388f0dcbc08b12
Rakantor <rakantor.dev@gmail.com>
2022-11-13 20:54:10 +0100

    Configure Nuxt to use the compiler-included Vue build

--- nuxt.config.js
+++ nuxt.config.js
@@ -100,6 +100,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend(config) {
+      config.resolve.alias.vue = 'vue/dist/vue.common'
+    },
     transpile: [
       'vee-validate/dist/rules'
     ],
